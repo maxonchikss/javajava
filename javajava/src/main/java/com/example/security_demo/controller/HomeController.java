dc363e04964c15da5ebafa317b55f18f71d8b6e9
Maxonchiks <nicichin.max@yandex.ru>
2025-12-05 22:22:20 +0300

    Spring server

---
+++ javajava/src/main/java/com/example/security_demo/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.security_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
+}
